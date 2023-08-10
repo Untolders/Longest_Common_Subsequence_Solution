dc7cee85481bcb1120985c34f818d08558921175
Anurag Singh <131255604+Untolders@users.noreply.github.com>
2023-08-10 12:42:07 +0530

    Create Longest_Common_Subsequence_Solution.cpp

---
+++ Longest_Common_Subsequence_Solution.cpp
@@ -0,0 +1,86 @@
+*********************************************************************  Question  *********************************************************************
+Given two strings, find the length of longest subsequence present in both of them. Both the strings are in uppercase latin alphabets.
+
+Example 1:
+
+Input:
+A = 6, B = 6
+str1 = ABCDGH
+str2 = AEDFHR
+Output: 3
+Explanation: LCS for input strings “ABCDGH” and “AEDFHR” is “ADH” of length 3.
+Example 2:
+
+Input:
+A = 3, B = 2
+str1 = ABC
+str2 = AC
+Output: 2
+Explanation: LCS of "ABC" and "AC" is "AC" of length 2.
+Your Task:
+Complete the function lcs() which takes the length of two strings respectively and two strings as input parameters and returns the length of the longest subsequence present in both of them.
+
+Expected Time Complexity : O(|str1|*|str2|)
+Expected Auxiliary Space: O(|str1|*|str2|)
+
+Constraints:
+1<=size(str1),size(str2)<=103
+
+
+  *********************************************************************  Solution  *********************************************************************
+
+  //{ Driver Code Starts
+#include<bits/stdc++.h>
+const int mod=1e9+7;
+using namespace std;
+
+// } Driver Code Ends
+// function to find longest common subsequence
+
+class Solution
+{
+    public:
+    //Function to find the length of longest common subsequence in two strings.
+    int lcs(int n, int m, string s1, string s2)
+    {
+        
+        vector<int>prev(m+1,0),cur(m+1,0);
+        for(int i = 1;i<=n;i++){
+         
+             for(int j = 1;j<=m;j++){
+                
+                if(s1[i-1] == s2[j-1]){
+                    cur[j] = 1 + prev[j-1];
+                } else {
+                  
+                    cur[j] = fmax(cur[j-1],prev[j]);
+                
+                    
+                }
+            
+                 
+             }
+            prev = cur;
+        }
+        return cur[m];
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t,n,m;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n>>m;          // Take size of both the strings as input
+        string s1,s2;
+        cin>>s1>>s2;        // Take both the string as input
+        Solution ob;
+        cout << ob.lcs(n, m, s1, s2) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
